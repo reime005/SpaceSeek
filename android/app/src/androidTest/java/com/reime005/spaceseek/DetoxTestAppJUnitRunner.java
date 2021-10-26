055b0d62ba2d5c279cb537e7f691dcec2a5c23b8
Marius Reimer <>
2021-10-26 19:43:23 +0200

    chore: Detox

---
+++ android/app/src/androidTest/java/com/reime005/spaceseek/DetoxTestAppJUnitRunner.java
@@ -0,0 +1,17 @@
+package com.reime005.spaceseek;
+
+import android.os.Bundle;
+
+import androidx.test.runner.AndroidJUnitRunner;
+
+public class DetoxTestAppJUnitRunner extends AndroidJUnitRunner {
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void finish(int resultCode, Bundle results) {
+        super.finish(resultCode, results);
+    }
+}
